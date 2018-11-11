ddecd8d237351a2952a4c3de7900516d186362aa
Paweł Kłósko <kloskopawel@gmail.com>
2018-11-11 14:56:37 +0100

    Added title message

--- Program.cs
+++ Program.cs
@@ -1,8 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+
 
 namespace NumberGuesser
 {
@@ -10,6 +7,23 @@ namespace NumberGuesser
     {
         static void Main(string[] args)
         {
+            //vars
+            string appName = "Number Guesser";
+            string appVersion = "1.0.0";
+            string appAuthor = "Kloskop";
+            string coAuthor = "Brad Traversy";
+
+            //setup text color
+            Console.ForegroundColor = ConsoleColor.DarkCyan;
+
+            Console.WriteLine("{0}: Version {1} by {2} (credits to: {3})", appName,appVersion,appAuthor,coAuthor);
+
+            //reset color
+            Console.ResetColor();
+
+            Console.WriteLine("What is your name?");
+            string input = Console.ReadLine();
+            Console.WriteLine("Hello {0}, let's play a game...",input);
         }
     }
 }
